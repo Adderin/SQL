b819ac7be4683aae512ef2ec27ea2c9e15c8d126
Adel <34869007+Adderin@users.noreply.github.com>
2019-09-23 21:37:43 +0200

    Update ExistsSimple.sql

--- ExistsSimple.sql
+++ ExistsSimple.sql
@@ -1,4 +1,4 @@
 SELECT d.id, d.name 
   FROM departments d
     WHERE EXISTS 
-      (SELECT s.name FROM sales s WHERE d.id = s.department_id AND price > 98.00);
+      (SELECT s.name FROM sales s WHERE d.id = s.department_id AND s.price > 98.00);
