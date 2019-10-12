a67fa8a3360e75b9565000ced36c4933f86ef61f
Adel <34869007+Adderin@users.noreply.github.com>
2019-10-13 00:58:02 +0200

    Update 6q_top10customers.sql

--- 6q_top10customers.sql
+++ 6q_top10customers.sql
@@ -1,6 +1,11 @@
-SELECT c.customer_id, c.email,
-       COUNT(p.amount) AS payments_count,
-       SUM(p.amount) AS total_amount
-       FROM customer c, payment p
-GROUP BY c.customer_id
+SELECT  c.customer_id,
+        email,
+        COUNT(c.customer_id) AS payments_count,
+        SUM(p.amount) AS total_amount
+FROM customer c
+JOIN
+payment p
+ON p.customer_id = c.customer_id 
+GROUP BY c.customer_id 
+ORDER BY payments_count DESC
 LIMIT 10;
